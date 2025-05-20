redhat-appstudio/rhtap-cli

Rename root of config.yaml

pkg/config/config.go
@@ -27,7 +27,7 @@ type Config struct {
 	cfs     *chartfs.ChartFS // embedded filesystem
 	payload []byte           // original configuration payload
 
-	Installer Spec `yaml:"rhtapCLI"` // root configuration for the installer
+	Installer Spec `yaml:"cli"` // root configuration for the installer
 }
 
 var (

pkg/config/config_test.go
@@ -44,7 +44,7 @@ func TestNewConfigFromFile(t *testing.T) {
 	t.Run("MarshalYAML and UnmarshalYAML", func(t *testing.T) {
 		payload, err := cfg.MarshalYAML()
 		g.Expect(err).To(o.Succeed())
-		g.Expect(string(payload)).To(o.ContainSubstring("rhtapCLI:"))
+		g.Expect(string(payload)).To(o.ContainSubstring("cli:"))
 
 		err = cfg.UnmarshalYAML()
 		g.Expect(err).To(o.Succeed())
@@ -52,6 +52,6 @@ func TestNewConfigFromFile(t *testing.T) {
 
 	t.Run("String", func(t *testing.T) {
 		payload := cfg.String()
-		g.Expect(string(payload)).To(o.ContainSubstring("rhtapCLI:"))
+		g.Expect(string(payload)).To(o.ContainSubstring("cli:"))
 	})
 }

pkg/subcmd/deploy.go
@@ -35,7 +35,7 @@ configuration to identify the features to be installed, and the dependencies to 
 resolved.
 
 The deployment configuration file describes the sequence of Helm charts to be
-applied, on the attribute 'rhtapCLI.dependencies[]'.
+applied, on the attribute 'cli.dependencies[]'.
 
 The platform configuration is rendered from the values template file
 (--values-template), this configuration payload is given to all Helm charts.

redhat-appstudio/tssc-cli

pkg/config/config.go
@@ -27,7 +27,7 @@ type Config struct {
 	cfs     *chartfs.ChartFS // embedded filesystem
 	payload []byte           // original configuration payload
 
-	Installer Spec `yaml:"rhtapCLI"` // root configuration for the installer
+	Installer Spec `yaml:"tssc"` // root configuration for the installer
 }
 
 var (

pkg/config/config_test.go
@@ -44,7 +44,7 @@ func TestNewConfigFromFile(t *testing.T) {
 	t.Run("MarshalYAML and UnmarshalYAML", func(t *testing.T) {
 		payload, err := cfg.MarshalYAML()
 		g.Expect(err).To(o.Succeed())
-		g.Expect(string(payload)).To(o.ContainSubstring("rhtapCLI:"))
+		g.Expect(string(payload)).To(o.ContainSubstring("tssc:"))
 
 		err = cfg.UnmarshalYAML()
 		g.Expect(err).To(o.Succeed())
@@ -52,6 +52,6 @@ func TestNewConfigFromFile(t *testing.T) {
 
 	t.Run("String", func(t *testing.T) {
 		payload := cfg.String()
-		g.Expect(string(payload)).To(o.ContainSubstring("rhtapCLI:"))
+		g.Expect(string(payload)).To(o.ContainSubstring("tssc:"))
 	})
 }

pkg/subcmd/deploy.go
@@ -35,7 +35,7 @@ configuration to identify the features to be installed, and the dependencies to 
 resolved.
 
 The deployment configuration file describes the sequence of Helm charts to be
-applied, on the attribute 'rhtapCLI.dependencies[]'.
+applied, on the attribute 'tssc.dependencies[]'.
 
 The platform configuration is rendered from the values template file
 (--values-template), this configuration payload is given to all Helm charts.