3822821d8d4240e830f6b5a21c04a51fb277febc
Raisa Varghese <ravarghe@redhat.com>
2025-10-09 17:09:46 +0530

    Renamed the file to constants.go

---
+++ pkg/constants/constants.go
@@ -0,0 +1,22 @@
+package constants
+
+import "fmt"
+
+const (
+	// AppName is the name of the application.
+	AppName = "tssc"
+
+	// OrgName is the name of the organization.
+	OrgName = "redhat-appstudio"
+
+	// Domain organization domain.
+	Domain = "github.com"
+)
+
+var (
+	// RepoURI is the reverse repository URI for the application.
+	RepoURI = fmt.Sprintf("%s.%s.%s", AppName, OrgName, Domain)
+)
+
+// Version is the application version, set at build time via ldflags.
+var Version = "v0.0.0-SNAPSHOT"
