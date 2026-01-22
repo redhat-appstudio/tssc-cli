a7b6dd59c76ed883ab5306a212a58dacda8cf527
Romain Arnaud <rarnaud@redhat.com>
2026-01-22 11:55:04 -0500

    fix: get-credentials.sh use the config on the cluster
    The script was previously using a local copy of the config that only existed
    if the deploy.sh script was used.
    
    rh-pre-commit.version: 2.3.2
    rh-pre-commit.check-secrets: ENABLED

---
+++ get-credentials.sh
@@ -0,0 +1,118 @@
+#!/usr/bin/env bash
+
+# shellcheck disable=SC2016
+
+set -o errexit
+set -o nounset
+set -o pipefail
+
+SCRIPT_DIR="$(
+    cd "$(dirname "$0")" >/dev/null
+    pwd
+)"
+
+usage() {
+    echo "
+Usage:
+    ${0##*/} [options]
+
+Optional arguments:
+    -d, --debug
+        Activate tracing/debug mode.
+    -h, --help
+        Display this message.
+
+Example:
+    ${0##*/}
+" >&2
+}
+
+parse_args() {
+    PROJECT_DIR="$(
+        cd "$(dirname "$SCRIPT_DIR")" >/dev/null
+        pwd
+    )"
+    while [[ $# -gt 0 ]]; do
+        case $1 in
+        -d | --debug)
+            set -x
+            DEBUG="--debug"
+            export DEBUG
+            ;;
+        -h | --help)
+            usage
+            exit 0
+            ;;
+        *)
+            echo "[ERROR] Unknown argument: $1"
+            usage
+            exit 1
+            ;;
+        esac
+        shift
+    done
+}
+
+h1() {
+    echo
+    echo "# $1 ##########################################################################" | cut -c -80
+}
+
+getNamespace() {
+    export PRODUCT="$1"
+    NAMESPACE="$(
+        oc get configmap -n tssc tssc-config -o jsonpath="{.data.config\.yaml}" \
+        | yq '.tssc.products[] | select(.name == strenv(PRODUCT)) | .namespace'
+    )"
+    oc get namespace "$NAMESPACE" > /dev/null 2>&1
+    return $?
+}
+
+konflux() {
+    if ! getNamespace "Konflux"; then
+        return
+    fi
+    h1 "Konflux"
+    echo "  - URL: $(oc get route -n "$NAMESPACE" --selector="konflux.konflux-ci.dev/component=ui" -o jsonpath="https://{.items[0].spec.host}")"
+}
+
+tas() {
+    if ! getNamespace "Trusted Artifact Signer"; then
+        return
+    fi
+    h1 "Trusted Artifact Signer"
+    ROUTE="$(oc get route -n $NAMESPACE --selector="app.kubernetes.io/name=rekor-search-ui" -o yaml | yq '.items[0].metadata.name')"
+    echo "  - URL: $(oc get route -n "$NAMESPACE" "$ROUTE" -o jsonpath="https://{.spec.host}")"
+}
+
+tpa() {
+    if ! getNamespace "Trusted Profile Analyzer"; then
+        return
+    fi
+    h1 "Trusted Profile Analyzer"
+    ROUTE="$(oc get route -n $NAMESPACE --selector="app.kubernetes.io/name=server" -o yaml | yq '.items[0].metadata.name')"
+    echo "  - URL: $(oc get route -n "$NAMESPACE" "$ROUTE" -o jsonpath="https://{.spec.host}")"
+}
+
+tssc() {
+    NAMESPACE="tssc"
+    h1 "TSSC"
+    echo "  - User: admin"
+    echo "  - Password: $(oc get secret -n "$NAMESPACE" tssc-realms-admin-user -o jsonpath="{.data.password}" | base64 -d)"
+}
+
+action() {
+    konflux
+    tas
+    tpa
+    tssc
+}
+
+main() {
+    parse_args "$@"
+    action
+}
+
+if [ "${BASH_SOURCE[0]}" == "$0" ]; then
+    main "$@"
+fi
